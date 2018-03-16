d046725b9920c1fbf7950876f91868a2127d5616
manuelli <lucas.t.manuelli@gmail.com>
2018-03-16 11:12:26 -0400

    move host code into ~/code folder

--- docker/docker_run.py
+++ docker/docker_run.py
@@ -34,7 +34,7 @@ if __name__=="__main__":
 		cmd += " --name %(container_name)s " % {'container_name': args.container}
 
 	cmd += " -e DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix:rw "     # enable graphics 
-	cmd += " -v %(source_dir)s:%(home_directory)s "  \
+	cmd += " -v %(source_dir)s:%(home_directory)s/code "  \
 		% {'source_dir': source_dir, 'home_directory': home_directory}	            # mount source
 	cmd += " -v ~/.ssh:%(home_directory)s/.ssh " % {'home_directory': home_directory}   # mount ssh keys
 	cmd += " -v /media:/media " #mount media
