19b28174b915462f747a24e359b699b7d96cecf6
manuelli <lucas.t.manuelli@gmail.com>
2018-03-19 10:53:14 -0400

    add more functionality to utils

--- modules/dense_correspondence/utils/utils.py
+++ modules/dense_correspondence/utils/utils.py
@@ -1,9 +1,52 @@
 # Basic I/O utils
 
 def getDictFromYamlFilename(filename):
+    """
+    Read data from a YAML files
+    """
     stream = file(filename)
     return yaml.load(stream)
 
 def saveToYaml(data, filename):
+    """
+    Save a data to a YAML file
+    """
     with open(filename, 'w') as outfile:
         yaml.dump(data, outfile, default_flow_style=False)
+
+
+def dictFromPosQuat(pos, quat):
+    """
+    Make a dictionary from position and quaternion vectors
+    """
+    d = dict()
+    d['translation'] = dict()
+    d['translation']['x'] = pos[0]
+    d['translation']['y'] = pos[1]
+    d['translation']['z'] = pos[2]
+
+    d['quaternion'] = dict()
+    d['quaternion']['w'] = quat[0]
+    d['quaternion']['x'] = quat[1]
+    d['quaternion']['y'] = quat[2]
+    d['quaternion']['z'] = quat[3]
+
+    return d
+
+
+def getQuaternionFromDict(d):
+    """
+    Get the quaternion from a dict describing a transform. The dict entry could be
+    one of orientation, rotation, quaternion depending on the convention
+    """
+    quat = None
+    quatNames = ['orientation', 'rotation', 'quaternion']
+    for name in quatNames:
+        if name in d:
+            quat = d[name]
+
+
+    if quat is None:
+        raise ValueError("Error when trying to extract quaternion from dict, your dict doesn't contain a key in ['orientation', 'rotation', 'quaternion']")
+
+    return quat
