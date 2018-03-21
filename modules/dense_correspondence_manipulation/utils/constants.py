887e6da3f0b2e70b666414b9b3435a7705137288
manuelli <lucas.t.manuelli@gmail.com>
2018-03-21 19:36:27 -0400

    update constants.py

--- modules/dense_correspondence_manipulation/utils/constants.py
+++ modules/dense_correspondence_manipulation/utils/constants.py
@@ -1,11 +1,5 @@
-from director import transformUtils
+import os
+import dense_correspondence_manipulation.utils.utils as utils
 
-CROP_BOX_DATA = dict()
-CROP_BOX_DATA['dimensions'] = [0.5, 0.7, 0.4]
-CROP_BOX_DATA['transform'] = transformUtils.transformFromPose([0.66757267, 0, 0.2], [1., 0., 0., 0.])
-
-
-cameraPoseTest = ([ 7.53674834e-01, -8.55423154e-19,  6.92103873e-01], [-0.34869616,  0.6090305 ,  0.62659914, -0.33891938])
-
-CAMERA_TO_WORLD = transformUtils.transformFromPose(cameraPoseTest[0], cameraPoseTest[1])
 DEPTH_IM_RESCALE = 4000.0
+CHANGE_DETECTION_CONFIG_FILE = os.path.join(utils.getDenseCorrespondenceSourceDir(), 'config', 'stations', 'RLG_iiwa_1', 'change_detection.yaml')
