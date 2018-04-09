a3f6fc108e60f4cb0521c096c5549ce6ea455d6a
manuelli <lucas.t.manuelli@gmail.com>
2018-04-09 19:13:15 -0400

    update background scene to new folder structure

--- modules/dense_correspondence_manipulation/utils/constants.py
+++ modules/dense_correspondence_manipulation/utils/constants.py
@@ -5,7 +5,7 @@ CHANGE_DETECTION_CONFIG_FILE = os.path.join(utils.getDenseCorrespondenceSourceDi
 
 CHANGE_DETECTION_BACKGROUND_SUBTRACTION_CONFIG_FILE = os.path.join(utils.getDenseCorrespondenceSourceDir(), 'config', 'stations', 'RLG_iiwa_1', 'change_detection_background_subtraction.yaml')
 
-BACKGROUND_SCENE_DATA_FOLDER = os.path.join(utils.getDenseCorrespondenceSourceDir(), 'data_volume', 'test', '14_background')
+BACKGROUND_SCENE_DATA_FOLDER = os.path.join(utils.getDenseCorrespondenceSourceDir(), 'data_volume', 'pdc','logs_proto', '14_background')
 
 DEPTH_IM_SCALE = 1000.0   # This represents that depth images are saved as uint16, where the integer value
                           # is depth in millimeters.  So this scale just converts millimeters to meters.
