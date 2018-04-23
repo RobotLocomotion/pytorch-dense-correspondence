2f11936eca3b92da98f45d3b8bb7698133b2158b
manuelli <lucas.t.manuelli@gmail.com>
2018-04-23 14:36:08 -0400

    add dense_correspondence to pythonpath

--- config/setup_environment.sh
+++ config/setup_environment.sh
@@ -1,6 +1,7 @@
 export DC_SOURCE_DIR=~/code
 
 export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/modules
+export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR
 export PATH=$PATH:$DC_SOURCE_DIR/bin
 export PATH=$PATH:$DC_SOURCE_DIR/modules/dense_correspondence_manipulation/scripts
 use_director(){
