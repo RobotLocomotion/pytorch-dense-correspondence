70effffac02ed9c96e6ccaaa92211e0f5eea3e9c
peteflorence <pete.florence@gmail.com>
2018-04-24 11:00:57 -0400

    add pdc path function in utils.py

--- modules/dense_correspondence_manipulation/utils/utils.py
+++ modules/dense_correspondence_manipulation/utils/utils.py
@@ -24,6 +24,8 @@ def saveToYaml(data, filename):
 def getDenseCorrespondenceSourceDir():
     return os.getenv("DC_SOURCE_DIR")
 
+def getPdcPath():
+    return os.path.join(getDenseCorrespondenceSourceDir(), "data_volume", "pdc")
 
 def dictFromPosQuat(pos, quat):
     """
