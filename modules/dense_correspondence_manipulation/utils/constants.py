bf3796772d5a0f61b342bc8be3250029ee50d9b4
manuelli <lucas.t.manuelli@gmail.com>
2018-04-25 20:31:46 -0400

    add image net normalization params to constants

--- modules/dense_correspondence_manipulation/utils/constants.py
+++ modules/dense_correspondence_manipulation/utils/constants.py
@@ -11,3 +11,6 @@ DEPTH_IM_SCALE = 1000.0   # This represents that depth images are saved as uint1
                           # is depth in millimeters.  So this scale just converts millimeters to meters.
 
 DEPTH_IM_RESCALE = 4000.0 # Only for visualizaiton purposes
+
+IMAGE_NET_MEAN = [0.485, 0.456, 0.406]
+IMAGE_NET_STD_DEV = [0.229, 0.224, 0.225]
