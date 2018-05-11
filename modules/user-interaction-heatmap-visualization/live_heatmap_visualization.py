cb4ef6bb19bc90a6e0b23b5445f3f46bb3d7708a
manuelli <lucas.t.manuelli@gmail.com>
2018-05-11 15:27:48 -0400

    more updates to heatmap visualization

--- modules/user-interaction-heatmap-visualization/live_heatmap_visualization.py
+++ modules/user-interaction-heatmap-visualization/live_heatmap_visualization.py
@@ -16,7 +16,7 @@ from dense_correspondence.evaluation.plotting import normalize_descriptor
 from dense_correspondence.network.dense_correspondence_network import DenseCorrespondenceNetwork
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../simple-pixel-correspondence-labeler"))
-from annotate_correspondences import label_colors, draw_reticle, pil_image_to_cv2, drawing_scale_config, numpy_to_cv2
+from annotate_correspondences import label_colors, draw_reticle, pil_image_to_cv2, drawing_scale_config, numpy_to_cv2, label_colors
 
 
 
@@ -29,23 +29,35 @@ EVAL_CONFIG = utils.getDictFromYamlFilename(eval_config_filename)
 
 class HeatmapVisualization(object):
 
-    def __init__(self):
-        self._dataset = SpartanDataset.make_default_caterpillar()
+    def __init__(self, config):
+        self._config = config
         self._dce = DenseCorrespondenceEvaluation(EVAL_CONFIG)
-        self._dcn = self._dce.load_network_from_config("caterpillar_background_0.5_3")
-
-        self._config = dict()
-        self._config["norm_diff_threshold"] = 0.25
+        self._load_networks()
         self._reticle_color = np.array([0, 0, 255])
-        self._blend_weight_original_image = 0.3
+
+    def _load_networks(self):
+        # we will use the dataset for the first network in the series
+        self._dcn_dict = dict()
+
+        self._dataset = None
+        self._network_reticle_color = dict()
+        for idx, network_name in enumerate(self._config["networks"]):
+            dcn = self._dce.load_network_from_config(network_name)
+            dcn.eval()
+            self._dcn_dict[network_name] = dcn
+            self._network_reticle_color[network_name] = label_colors[idx]
+
+            if self._dataset is None:
+                self._dataset = dcn.load_training_dataset()
 
     def get_random_image_pair(self, randomize_image=False):
         object_id = self._dataset.get_random_object_id()
         scene_name_a = self._dataset.get_random_single_object_scene_name(object_id)
         scene_name_b = self._dataset.get_different_scene_for_object(object_id, scene_name_a)
 
-        if randomize_image:
-            pass
+        if self._config["randomize_images"]:
+            image_a_idx = self._dataset.get_random_image_index(scene_name_a)
+            image_b_idx = self._dataset.get_random_image_index(scene_name_b)
         else:
             image_a_idx = 0
             image_b_idx = 0
@@ -53,32 +65,52 @@ class HeatmapVisualization(object):
         return scene_name_a, scene_name_b, image_a_idx, image_b_idx
 
     def _get_new_images(self):
+        """
+        Gets a new pair of images
+        :return:
+        :rtype:
+        """
+
+        if random.random() < 0.5:
+            self._dataset.set_train_mode()
+        else:
+            self._dataset.set_test_mode()
+
         scene_name_1, scene_name_2, image_1_idx, image_2_idx = self.get_random_image_pair()
 
-        img1_pil = self._dataset.get_rgb_image_from_scene_name_and_idx(scene_name_1, image_1_idx)
-        img2_pil = self._dataset.get_rgb_image_from_scene_name_and_idx(scene_name_2, image_2_idx)
+        self.img1_pil = self._dataset.get_rgb_image_from_scene_name_and_idx(scene_name_1, image_1_idx)
+        self.img2_pil = self._dataset.get_rgb_image_from_scene_name_and_idx(scene_name_2, image_2_idx)
 
-        rgb_1_tensor = self._dataset.rgb_image_to_tensor(img1_pil)
-        rgb_2_tensor = self._dataset.rgb_image_to_tensor(img2_pil)
+        self._compute_descriptors()
 
-        self.res_a = self._dcn.forward_single_image_tensor(rgb_1_tensor).data.cpu().numpy()
-        self.res_b = self._dcn.forward_single_image_tensor(rgb_2_tensor).data.cpu().numpy()
-
-        self.img1_descriptors = numpy_to_cv2(self.res_a)
-        self.img2_descriptors = numpy_to_cv2(self.res_b)
-
-        self.img1 = pil_image_to_cv2(img1_pil)
-        self.img1_gray = cv2.cvtColor(self.img1, cv2.COLOR_RGB2GRAY)/255.0
-        self.img2 = pil_image_to_cv2(img2_pil)
-        self.img2_gray = cv2.cvtColor(self.img2, cv2.COLOR_RGB2GRAY)/255.0
-
-        cv2.imshow('image1', self.img1)
-        cv2.imshow('image2', self.img2)
-
-        self.find_best_match_from_image_1(None, 0, 0, None, None)
-        self.find_best_match_from_image_2(None, 0, 0, None, None)
+        # self.rgb_1_tensor = self._dataset.rgb_image_to_tensor(img1_pil)
+        # self.rgb_2_tensor = self._dataset.rgb_image_to_tensor(img2_pil)
 
 
+    def _compute_descriptors(self):
+        """
+        Computes the descriptors for image 1 and image 2 for each network
+        :return:
+        :rtype:
+        """
+        self.img1 = pil_image_to_cv2(self.img1_pil)
+        self.img2 = pil_image_to_cv2(self.img2_pil)
+        self.rgb_1_tensor = self._dataset.rgb_image_to_tensor(self.img1_pil)
+        self.rgb_2_tensor = self._dataset.rgb_image_to_tensor(self.img2_pil)
+        self.img1_gray = cv2.cvtColor(self.img1, cv2.COLOR_RGB2GRAY) / 255.0
+        self.img2_gray = cv2.cvtColor(self.img2, cv2.COLOR_RGB2GRAY) / 255.0
+
+        cv2.imshow('source', self.img1)
+        cv2.imshow('target', self.img2)
+
+        self._res_a = dict()
+        self._res_b = dict()
+        for network_name, dcn in self._dcn_dict.iteritems():
+            self._res_a[network_name] = dcn.forward_single_image_tensor(self.rgb_1_tensor).data.cpu().numpy()
+            self._res_b[network_name] = dcn.forward_single_image_tensor(self.rgb_2_tensor).data.cpu().numpy()
+
+
+        self.find_best_match(None, 0, 0, None, None)
 
     def scale_norm_diffs_to_make_heatmap(self, norm_diffs):
         """
@@ -94,83 +126,53 @@ class HeatmapVisualization(object):
         threshold = self._config["norm_diff_threshold"]
         heatmap = np.copy(norm_diffs)
         greater_than_threshold = np.where(norm_diffs > threshold)
-        heatmap = heatmap / threshold * 0.5 # linearly scale [0, threshold] to [0, 0.5]
+        heatmap = heatmap / threshold * self._config["heatmap_vis_upper_bound"] # linearly scale [0, threshold] to [0, 0.5]
         heatmap[greater_than_threshold] = 1 # greater than threshold is set to 1
         heatmap = heatmap.astype(self.img1_gray.dtype)
         return heatmap
 
 
-    def find_best_match_from_image_1(self, event,u,v,flags,param):
+    def find_best_match(self, event,u,v,flags,param):
+
         """
-        This should update the heatmap for image 2, self.img2_heatmap
+        For each network, find the best match in the target image to point highlighted
+        with reticle in the source image. Displays the result
         :return:
         :rtype:
         """
 
         img_1_with_reticle = np.copy(self.img1)
         draw_reticle(img_1_with_reticle, u, v, self._reticle_color)
-        cv2.imshow("image1", img_1_with_reticle)
+        cv2.imshow("source", img_1_with_reticle)
 
-        best_match_uv, best_match_diff, norm_diffs = DenseCorrespondenceNetwork.find_best_match((u, v), self.res_a, self.res_b)
-
-        print "best_match_diff", best_match_diff
-
-        self.img2_heatmap = self.scale_norm_diffs_to_make_heatmap(norm_diffs)
-        draw_reticle(self.img2_heatmap, best_match_uv[0], best_match_uv[1], self._reticle_color)
-
-        alpha = self._blend_weight_original_image
-        beta = 1 - self._blend_weight_original_image
-
-        blended = cv2.addWeighted(self.img2_gray, alpha,
-                                  self.img2_heatmap, beta, 0)
+        alpha = self._config["blend_weight_original_image"]
+        beta = 1 - alpha
 
         img_2_with_reticle = np.copy(self.img2)
-        draw_reticle(img_2_with_reticle, best_match_uv[0], best_match_uv[1], self._reticle_color)
 
-        cv2.imshow("image2", img_2_with_reticle)
+        print "\n\n"
+        for network_name in self._dcn_dict:
+            res_a = self._res_a[network_name]
+            res_b = self._res_b[network_name]
+            best_match_uv, best_match_diff, norm_diffs = \
+                DenseCorrespondenceNetwork.find_best_match((u, v), res_a, res_b)
 
-        cv2.imshow('image2_heatmap', blended)
+            print "%s best match diff: %.3f" %(network_name, best_match_diff)
+            heatmap = self.scale_norm_diffs_to_make_heatmap(norm_diffs)
 
-    def find_best_match_from_image_2(self, event,u,v,flags,param):
-        """
-        This should update the heatmap for image 2, self.img2_heatmap
-        :return:
-        :rtype:
-        """
+            reticle_color = self._network_reticle_color[network_name]
+            draw_reticle(heatmap, best_match_uv[0], best_match_uv[1], reticle_color)
+            draw_reticle(img_2_with_reticle, best_match_uv[0], best_match_uv[1], reticle_color)
+            blended = cv2.addWeighted(self.img2_gray, alpha, heatmap, beta, 0)
+            cv2.imshow(network_name, blended)
 
-        img_2_with_reticle = np.copy(self.img2)
-        draw_reticle(img_2_with_reticle, u, v, self._reticle_color)
-        cv2.imshow("image2", img_2_with_reticle)
-
-        best_match_uv, best_match_diff, norm_diffs = DenseCorrespondenceNetwork.find_best_match((u, v), self.res_b, self.res_a)
-
-        print "best_match_diff", best_match_diff
-
-        self.img1_heatmap = self.scale_norm_diffs_to_make_heatmap(norm_diffs)
-        draw_reticle(self.img1_heatmap, best_match_uv[0], best_match_uv[1], self._reticle_color)
-
-
-        alpha = self._blend_weight_original_image
-        beta = 1 - self._blend_weight_original_image
-        blended = cv2.addWeighted(self.img1_gray, alpha,
-                                  self.img1_heatmap, beta, 0)
-
-        cv2.imshow('image1_heatmap', blended)
-
-        img_1_with_reticle = np.copy(self.img1)
-        draw_reticle(img_1_with_reticle, best_match_uv[0], best_match_uv[1], self._reticle_color)
-
-        cv2.imshow("image1", img_1_with_reticle)
-
+        cv2.imshow("target", img_2_with_reticle)
 
     def run(self):
-        cv2.namedWindow('image1')
-        cv2.setMouseCallback('image1', self.find_best_match_from_image_1)
-        cv2.namedWindow('image1_heatmap')
+        self._get_new_images()
+        cv2.namedWindow('target')
+        cv2.setMouseCallback('source', self.find_best_match)
 
-        cv2.namedWindow('image2')
-        cv2.setMouseCallback('image2', self.find_best_match_from_image_2)
-        cv2.namedWindow('image2_heatmap')
         self._get_new_images()
 
         while True:
@@ -180,10 +182,20 @@ class HeatmapVisualization(object):
             elif k == ord('n'):
                 print "HEY"
                 self._get_new_images()
-
+            elif k == ord('s'):
+                print "HEY"
+                img1_pil = self.img1_pil
+                img2_pil = self.img2_pil
+                self.img1_pil = img2_pil
+                self.img2_pil = img1_pil
+                self._compute_descriptors()
 
 if __name__ == "__main__":
-    heatmap_vis = HeatmapVisualization()
+    dc_source_dir = utils.getDenseCorrespondenceSourceDir()
+    config_file = os.path.join(dc_source_dir, 'config', 'dense_correspondence', 'heatmap_vis', 'heatmap.yaml')
+    config = utils.getDictFromYamlFilename(config_file)
+
+    heatmap_vis = HeatmapVisualization(config)
     print "starting heatmap vis"
     heatmap_vis.run()
     cv2.destroyAllWindows()
