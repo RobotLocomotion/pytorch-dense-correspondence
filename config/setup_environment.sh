1c26d5b6e866aa06627af4e9805bcb19a9665109
manuelli <lucas.t.manuelli@gmail.com>
2018-07-31 17:47:12 -0400

    add director source build

--- config/setup_environment.sh
+++ config/setup_environment.sh
@@ -5,7 +5,7 @@ export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR
 export PATH=$PATH:$DC_SOURCE_DIR/bin
 export PATH=$PATH:$DC_SOURCE_DIR/modules/dense_correspondence_manipulation/scripts
 use_director(){
-    export PATH=$PATH:~/director/bin
+    export PATH=$PATH:$DC_SOURCE_DIR/build/director/install/bin
 }
 
 export -f use_director
