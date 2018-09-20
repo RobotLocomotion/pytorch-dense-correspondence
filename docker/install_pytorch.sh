8cc41ec17b8f0e8cd68db25d5017bd66d9db7f0f
manuelli <lucas.t.manuelli@gmail.com>
2018-09-20 17:43:27 -0400

    fix pytorch version numbers

--- docker/install_pytorch.sh
+++ docker/install_pytorch.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-sudo pip install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp27-cp27mu-linux_x86_64.whl 
-sudo pip install torchvision
+sudo pip install torch==0.3.1
+sudo pip install torchvision==0.2.1
 
 sudo apt-get install python3-pip
 sudo pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
