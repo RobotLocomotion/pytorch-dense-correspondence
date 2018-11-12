abb5f057748f18749bdee77657139d6bc9f9a8a4
manuelli <lucas.t.manuelli@gmail.com>
2018-11-12 17:05:44 -0500

    improve documentation

--- modules/dense_correspondence_manipulation/fusion/fusion_reconstruction.py
+++ modules/dense_correspondence_manipulation/fusion/fusion_reconstruction.py
@@ -323,7 +323,7 @@ class TSDFReconstruction(FusionReconstruction):
             foreground_reconstruction_filename =\
                 self.dataset_structure.foreground_fusion_reconstruction_file
             if not os.path.isfile(foreground_reconstruction_filename):
-                raise ValueError("Foreground reconstruction not found")
+                raise ValueError("Foreground reconstruction not found, you may need to re-run change detection to generate this file.")
 
             self.poly_data = ioUtils.readPolyData(foreground_reconstruction_filename)
         else:
