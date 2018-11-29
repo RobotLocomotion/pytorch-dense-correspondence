b5d164e9b527f058cb5cd737ce1ac915e7f4e437
manuelli <lucas.t.manuelli@gmail.com>
2018-11-28 20:50:27 -0500

    pin tensorboard to 1.8.0

--- docker/install_more.sh
+++ docker/install_more.sh
@@ -9,14 +9,19 @@ sudo pip install scipy
 sudo pip install imageio
 
 sudo pip install scikit-image
-sudo pip install tensorboard
+
+sudo pip install tensorboard_logger \
+    tensorflow
+
+# seems that we need this version of tensorboard
+# maybe because tensorboard_logger is not compatible 
+# with newer versions of tensorboard?
+sudo pip install tensorboard==1.8.0
 
 sudo pip install sklearn
 
 sudo pip install opencv-contrib-python
 
-sudo pip install tensorboard_logger \
-    tensorflow
 
 sudo apt install python-tk \
     ffmpeg
