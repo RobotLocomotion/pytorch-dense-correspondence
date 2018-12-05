25af6153fb86e7872532b32830db307f42e33c38
wei <gaowei19951004@gmail.com>
2018-12-05 14:51:46 -0500

    change to another file

--- docker/install_more.sh
+++ docker/install_more.sh
@@ -25,13 +25,3 @@ sudo pip install opencv-contrib-python
 
 sudo apt install python-tk \
     ffmpeg
-
-# The dependency for poser
-sudo apt install libpcl-dev
-sudo apt install libopencv-dev
-sudo apt install libproj-dev
-sudo apt install libgoogle-glog-dev
-
-# The google test need to build
-sudo apt install libgtest-dev
-sudo mkdir /usr/src/gtest/build && cmake /usr/src/gtest/ -B/usr/src/gtest/build && sudo make --directory=/usr/src/gtest/build && sudo cp /usr/src/gtest/build/*.a /usr/lib
--- docker/pytorch-dense-correspondence.dockerfile
+++ docker/pytorch-dense-correspondence.dockerfile
@@ -28,6 +28,9 @@ RUN yes "Y" | /tmp/install_pytorch.sh
 COPY ./install_more.sh /tmp/install_more.sh
 RUN yes "Y" | /tmp/install_more.sh
 
+COPY ./install_poser.sh /tmp/install_poser.sh
+RUN yes "Y" | /tmp/install_poser.sh
+
 # install director
 COPY ./install_director.sh /tmp/install_director.sh
 RUN yes "Y" | /tmp/install_director.sh
