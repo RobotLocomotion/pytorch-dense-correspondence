bd77bc7699b192c63183c5e82e0a8889a44e9198
manuelli <lucas.t.manuelli@gmail.com>
2019-05-27 09:19:57 -0400

    switch to pytorch 1.1

--- docker/install_pytorch.sh
+++ docker/install_pytorch.sh
@@ -2,8 +2,12 @@
 
 set -euxo pipefail
 
-# pytorch 1.0, CUDA 10
-pip install numpy torchvision_nightly
-pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cu100/torch_nightly.html
+# pytorch 1.1, CUDA 10
+pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27mu-linux_x86_64.whl
+pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27mu-linux_x86_64.whl
+
+# if the above command does not work, then you have python 2.7 UCS2, use this command
+# pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27m-linux_x86_64.whl
+# pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27m-linux_x86_64.whl
 
 
