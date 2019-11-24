31b8f48e00cfec95a1185213f7a2d6cbaf1f2d48
manuelli <lucas.t.manuelli@gmail.com>
2019-11-24 11:16:41 -0500

    don't automatically set environment variables when setup_environment is called

--- config/setup_environment.sh
+++ config/setup_environment.sh
@@ -1,9 +1,3 @@
-export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR/modules
-export PYTHONPATH=$PYTHONPATH:$DC_SOURCE_DIR
-export PATH=$PATH:$DC_SOURCE_DIR/bin
-export PATH=$PATH:$DC_SOURCE_DIR/modules/dense_correspondence_manipulation/scripts
-
-
 #!/bin/bash
 set -e
 
