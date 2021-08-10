c2f0fc42f1a893faa4ae2669a8241018d200fcad
nikola <gicko3@gmail.com>
2021-08-10 10:07:39 +0200

    Updated package versions

--- docker/install_pytorch.sh
+++ docker/install_pytorch.sh
@@ -7,7 +7,7 @@ pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27mu-linux
 pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27mu-linux_x86_64.whl
 
 # if the above command does not work, then you have python 2.7 UCS2, use this command
-# pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27m-linux_x86_64.whl
-# pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27m-linux_x86_64.whl
+#pip install https://download.pytorch.org/whl/cu100/torch-1.1.0-cp27-cp27m-linux_x86_64.whl
+#pip install https://download.pytorch.org/whl/cu100/torchvision-0.3.0-cp27-cp27m-linux_x86_64.whl
 
 
